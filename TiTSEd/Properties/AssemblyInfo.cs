91e718552e15673839752fef36e993c044d1cadf
Chase <chasesan@gmail.com>
2016-05-16 21:26:25 -0500

    Version!

--- TiTSEd/Properties/AssemblyInfo.cs
+++ TiTSEd/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Windows;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("TiTsEd")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Save Editor for Trials in Tainted Space")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("TiTsEd")]
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.3.0.*")]
-[assembly: AssemblyFileVersion("0.3.0.0")]
+[assembly: AssemblyVersion("0.4.0.*")]
+[assembly: AssemblyFileVersion("0.4.0.0")]
