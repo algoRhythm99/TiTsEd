39b4062aaf96a98df26a8cf1837fad9548098c06
tehHax0r793 <tehhax0r793@gmail.com>
2016-07-24 01:02:42 -0500

    Adding fertility editing

--- TiTsEd/ViewModel/CharacterVM.cs
+++ TiTsEd/ViewModel/CharacterVM.cs
@@ -574,6 +574,11 @@ namespace TiTsEd.ViewModel {
             set { SetValue("girlCumType", value); }
         }
 
+        public int Fertility {
+            get { return GetInt("fertilityRaw"); }
+            set { SetValue("fertilityRaw", value); }
+        }
+
         public double ClitLength {
             get { return GetDouble("clitLength"); }
             set { SetValue("clitLength", value); }
