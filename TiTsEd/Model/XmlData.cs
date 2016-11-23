37a3e64a1d3bd5cb84b4a551655c3ea445282437
tehHax0r793 <tehhax0r793@gmail.com>
2016-11-23 16:05:58 -0600

    Use a single list for fluid types

--- TiTsEd/Model/XmlData.cs
+++ TiTsEd/Model/XmlData.cs
@@ -177,21 +177,14 @@ namespace TiTsEd.Model {
         [XmlArray, XmlArrayItem("GenitalSpotType")]
         public XmlEnum[] GenitalSpotTypes { get; set; }
 
-        [XmlArray, XmlArrayItem("MilkType")]
-        public XmlEnum[] MilkTypes { get; set; }
-
-        [XmlArray, XmlArrayItem("CumType")]
-        public XmlEnum[] CumTypes { get; set; }
-
-        [XmlArray, XmlArrayItem("GirlCumType")]
-        public XmlEnum[] GirlCumTypes { get; set; }
+        [XmlArray, XmlArrayItem("FluidType")]
+        public XmlEnum[] FluidTypes { get; set; }
 
         [XmlArray, XmlArrayItem("NippleType")]
         public XmlEnum[] NippleTypes { get; set; }
 
         [XmlArray, XmlArrayItem("DickNippleType")]
         public XmlEnum[] DickNippleTypes { get; set; }
-
 
         [XmlArray, XmlArrayItem("VaginaType")]
         public XmlEnum[] VaginaTypes { get; set; }
