1076d80356cd4ed0226cfc3fbe56f4537b379754
tehHax0r793 <tehhax0r793@gmail.com>
2018-10-31 01:27:32 -0500

    Updating versions

--- TiTsEd/Properties/AssemblyInfo.cs
+++ TiTsEd/Properties/AssemblyInfo.cs
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.27.8.*")]
-[assembly: AssemblyFileVersion("0.27.8.0")]
+[assembly: AssemblyVersion("0.27.9.*")]
+[assembly: AssemblyFileVersion("0.27.9.0")]
