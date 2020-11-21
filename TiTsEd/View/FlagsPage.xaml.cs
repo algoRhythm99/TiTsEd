56139c109409284b0504c2a487f2679bcacf9b32
algoRhythm99 <tehhax0r793@gmail.com>
2020-11-21 14:23:58 -0600

    Split Status and Flags

---
+++ TiTsEd/View/FlagsPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows.Controls;
+using System.Windows.Input;
+using TiTsEd.ViewModel;
+
+namespace TiTsEd.View {
+    /// <summary>
+    /// Interaction logic for FlagsPage.xaml
+    /// </summary>
+    public partial class FlagsPage : UserControl {
+        public FlagsPage() {
+            InitializeComponent();
+        }
+
+        private void Add_Flag_Click(object sender, CanExecuteRoutedEventArgs e)
+        {
+        }
+    }
+}
